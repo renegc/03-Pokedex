4c6347624633971aa8b6794350a51561c7cf6d8b
René González <61342065+renegc@users.noreply.github.com>
2022-11-08 15:49:02 -0600

    antes de por lotes

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import axios, {AxiosInstance} from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+ 
+  async executedSeed() {
+
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    data.results.forEach(({name, url}) => {
+
+      const sergemnts = url.split('/')
+      const no: number = +sergemnts [sergemnts.length - 2]
+
+      console.log(name, no)
+    })
+   
+    return data.results;
+  }
+}
